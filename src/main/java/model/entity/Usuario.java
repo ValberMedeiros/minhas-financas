4222d7b0cad8cdf913d2878cbcbc7c35e7300fc1
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-09 20:47:52 -0300

    Mapeando a entidade Usuário.

---
+++ src/main/java/model/entity/Usuario.java
@@ -0,0 +1,35 @@
+package model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario", schema = "minhasfinancas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+}
