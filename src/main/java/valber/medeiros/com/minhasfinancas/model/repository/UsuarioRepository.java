b0106794276da096760d0b4fe5043db21dd6c088
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-09 21:27:16 -0300

    Criando os Repository's de Usuario e Lancamento.

---
+++ src/main/java/valber/medeiros/com/minhasfinancas/model/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package valber.medeiros.com.minhasfinancas.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import valber.medeiros.com.minhasfinancas.model.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
