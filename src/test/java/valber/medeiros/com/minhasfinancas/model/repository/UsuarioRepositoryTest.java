0c965ffdb47245e3a169a19361598106b345b1f9
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-09 23:35:33 -0300

    Refatorando imports.

--- src/test/java/valber/medeiros/com/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/valber/medeiros/com/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,22 +1,22 @@
 package valber.medeiros.com.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import valber.medeiros.com.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
-public class UsuarioRepositoryTest {
+@ExtendWith(SpringExtension.class)
+class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository repository;
 
     @Test
-    public void deveVerificarAExistenciaDeUmEmail() {
+    void deveVerificarAExistenciaDeUmEmail() {
         //Cenário
         Usuario usuario = Usuario.builder()
                 .nome("usuario")
